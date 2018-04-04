4d0e63463ad8e0b0170a6701ef0f9a707fcc40eb
phi <phi@gress.ly>
2018-04-04 21:16:50 +0200

    Grobe Schreibfehler inkl. vertauschung von Strom und Spannung bei Transistor behoben

--- site/artikel_html/transistor.php
+++ site/artikel_html/transistor.php
@@ -1,22 +1,22 @@
 <h3>Aufbau</h3>
 
-<p>Der Transistor hat drei Metallstäbe; eines Rechts, eines Links und eines in der Mitte.<br />
+<p>Der Transistor hat drei Metallstäbe; einer Rechts, einer Links und einer in der Mitte.<br />
 Der mittlere Stab ist im Inneren mit einer Art Wiederstand verbunden.
 Die Stäbe links und rechts sind mit speziell behandeltem Silizium verbunden.</p>
 <?php includeImage(); ?>
                                             
 <h3>Funktionsweise</h3>
 <p>
-Wenn der mittlere Metallstab nicht unter Strom ist,
+Wenn der mittlere Metallstab nicht unter Spannung ist,
 kann kein Strom von links nach rechts fliessen und umgekehrt. 
-Wenn der mittlere Metallstab unter Strom ist, kann Strom von links nach rechts fliessen. Die Spannung wird dabei sogar um ca. das 200fache verstärkt! 
+Wenn der mittlere Metallstab unter Spannung ist, kann Strom von links nach rechts fliessen. Der Strom wird dabei sogar um ca. das 200fache verstärkt! 
 </p>
 
 <h3>Verwendung</h3>
 <p>
-Die Mainboard (auch Motherboard genannt) verwendet selbst Transistoren,
+Das Mainboard (auch Motherboard genannt) verwendet selbst Transistoren,
 jeder Prozessor enthält tausende von Transistoren.
-Jede Grafikkarte enthält somit auch tausende von Tronsistoren, der Arbeitsspeicher ebenso, da der Speicher von einem Mikroprozessor gesteuert wird.</p>
+Jede Grafikkarte enthält somit auch tausende von Transistoren.</p>
 
 <h3>Weitere Informationen</h3>
 <p>
