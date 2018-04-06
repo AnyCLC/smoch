ac0ff9ff8aaf02f17dc17d20bd8e74b8fda68bc1
phi <phi@gress.ly>
2018-04-06 13:35:15 +0200

    Ergänzungen in der DB, insb. Autor, Keywords

--- db/content.sql
+++ db/content.sql
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Datenbank: `smoch2`
+-- Database: `smoch`
 --
 
 -- --------------------------------------------------------
@@ -50,13 +50,13 @@ CREATE TABLE `content` (
 --
 
 INSERT INTO `content` (`ID`, `IDurl`, `Titel`, `Jahrzahl`, `Objekt_Jahr`, `Objekt_Modell`, `BildPfad`, `BildLegende`, `Vorher`, `Nachher`, `Vorhanden`, `Autor`, `Descriptions`, `Keywords`) VALUES
-(49, 'abakus', 'Abakus', '-2500', '', 'Chinesischer Abakus', 'abakus.png', 'Abakus', NULL, 'pascaline', 1, '', 'Die Geschichte des Abakus', ''),
-(50, 'pascaline', 'Pascaline', '1642', '1652', 'Pascaline', 'pascaline.png', 'Pascaline', 'abakus', 'binaer', 0, '', 'Die Geschichte der Pascaline', ''),
-(51, 'binaer', 'Binärsystem', '1697', '', '', 'binaer.png', 'Binärsystem', 'pascaline', 'babbage', 1, '', 'Die Geschichte des Binärsystems', ''),
-(52, 'babbage', 'Differenzenmaschine', '1822', '', '', 'babbage.png', 'Differenzenmaschine', 'binaer', 'mechanik', 0, '', 'Die Geschichte der Differenzmaschine', ''),
-(53, 'mechanik', 'Mechanischer Rechner', '1847', '', '', 'zahlenschieber.png', 'Zahlenschieber', 'babbage', 'relais', 1, '', 'Die Geschichte des menchanischen Rechners', ''),
+(49, 'abakus', 'Abakus', '-2500', 'ca 1970', 'Chinesischer Abakus', 'abakus.png', 'Abakus', NULL, 'pascaline', 1, '', 'Die Geschichte des Abakus', 'chinesischer Zählrahmen'),
+(50, 'pascaline', 'Pascaline', '1642', '1652', 'Pascaline', 'pascaline.png', 'Pascaline', 'abakus', 'binaer', 0, '', 'Die Geschichte der Pascaline', 'Blaise Pascal, Mechanische Rechenmaschine'),
+(51, 'binaer', 'Binärsystem', '1697', '', '', 'binaer.png', 'Binärsystem', 'pascaline', 'babbage', 1, 'Lisa Kalberer, Philipp Gressly Freimann', 'Die Geschichte des Binärsystems', 'Leibniz, Dualsystem'),
+(52, 'babbage', 'Differenzenmaschine', '1822', '', 'Nachbildung im Da Vinci Museum Mailand', 'babbage.png', 'Differenzenmaschine', 'binaer', 'mechanik', 0, '', 'Die Geschichte der Differenzmaschine', 'Babbage'),
+(53, 'mechanik', 'Mechanischer Rechner', '1847', 'ca. 1950', 'Addiator', 'zahlenschieber.png', 'Zahlenschieber', 'babbage', 'relais', 1, '', 'Die Geschichte des menchanischen Rechners', ''),
 (54, 'relais', 'Relais', '1835', '', '', 'relais.png', 'Relais', 'mechanik', 'halbleiter', 1, '', 'Die Geschichte des Relais\'', ''),
-(55, 'halbleiter', 'Halbleiter', '1874', '', '', 'halbleiter.png', 'Halbleiter', 'relais', 'transistor', 1, '', 'Die  Geschichte von Halbleitern', ''),
+(55, 'halbleiter', 'Halbleiter', '1874', '', 'Element Silizium', 'halbleiter.png', 'Halbleiter', 'relais', 'transistor', 1, '', 'Die  Geschichte von Halbleitern', ''),
 (56, 'transistor', 'Transistor', '1934', '1997', 'Transistor BC', 'transistor.png', 'Transistor', 'halbleiter', 'halbaddierer', 1, '', 'Die Geschichte des Transistors', ''),
 (57, 'halbaddierer', 'Halbaddierer', '1937', '', '', 'halbaddierer.png', 'Halbaddierer', 'transistor', 'roehren', 1, '', 'Die Geschichte des Halbaddierers', ''),
 (58, 'roehren', 'Röhrenrechner', '1940', '', '', 'roehren.png', 'Röhrenrechner', 'halbaddierer', 'rechnerchip', 1, '', 'Die Geschichte des Röhrenrechners', ''),
@@ -64,41 +64,41 @@ INSERT INTO `content` (`ID`, `IDurl`, `Titel`, `Jahrzahl`, `Objekt_Jahr`, `Objek
 (60, 'integrierterRechner', 'Integrierter Schaltkreis', '1959', '', '', 'integrierter.png', 'Integrierter Schaltkreis', 'rechnerchip', 'rwafer', 1, '', 'Die Geschichte des integrierten Schaltkreises', ''),
 (61, 'rwafer', 'Wafer', '1960', '', '', 'wafer.png', 'Wafer', 'integrierterRechner', 'taschenrechner', 1, '', 'Die Geschichte des Wafers', ''),
 (62, 'taschenrechner', 'Taschenrechner', '1967', '', '', 'taschenrechner.png', 'Taschenrechner', 'rwafer', 'pc', 1, '', 'Die Geschichte des Taschenrechners', ''),
-(63, 'pc', 'Personal Computer', '1975', '2008', 'Asus Eee PC 1000', 'pc.png', 'Personal Computer', 'taschenrechner', 'tablet', 1, '', 'Die Geschichte des PC\'s', ''),
+(63, 'pc', 'Personal Computer', '1975', '2008', 'Asus Eee PC 1000', 'pc.png', 'Personal Computer', 'taschenrechner', 'tablet', 1, '', 'Die Geschichte des PC\'s', 'PC, Laptop'),
 (64, 'tablet', 'Tablet', '1989', '2011', 'iPad 2', 'tablet.png', 'Tablet', 'pc', 'smartphone', 1, '', 'Die Geschichte des Tablets', ''),
 (65, 'smartphone', 'Smartphone', '1994', '1998', 'Nokia NSM-1NY', 'smartphone.png', 'Smartphone', 'tablet', 'smartwatch', 1, '', 'Die Geschichte des Smartphones', ''),
 (66, 'smartwatch', 'Smartwatch', '2003', '', '', 'smartwatch.png', 'Smartwatch', 'smartphone', NULL, 0, '', 'Die Geschichte der Smartwatch', ''),
-(67, 'amphore', 'Amphore', '-500', '', '', 'amphore.png', 'Amphore', NULL, 'lochkarte', 1, '', 'Die Geschichte der Amphore', ''),
+(67, 'amphore', 'Amphore', '-500', '2016', 'Nachbildung etruskischer Amphore', 'amphore.png', 'Amphore', NULL, 'lochkarte', 1, '', 'Die Geschichte der Amphore', 'Speicher'),
 (68, 'lochkarte', 'Lochkarte', '1874', '', '', 'lochkarte.png', 'Lochkarte', 'amphore', 'magnetband', 1, '', 'Die Geschichte der Lochkarte', ''),
 (69, 'magnetband', 'Magnetband', '1930', '1990', 'Magnetband BASF', 'magnetband.png', 'Magnetband', 'lochkarte', 'kernspeicher', 1, '', 'Die Geschichte des Magnetbandes', ''),
-(70, 'kernspeicher', 'Kernspeicher', '1949', '', '', 'kernspeicher.png', 'Kernspeicher', 'magnetband', 'festplatte', 1, '', 'Die Geschichte des Kernspeichers', ''),
-(71, 'festplatte', 'Festplatte', '1956', '1998', '', 'festplatte.png', 'Festplatte', 'kernspeicher', 'speicherchip', 1, '', 'Die Geschichte der Festplatte', ''),
+(70, 'kernspeicher', 'Kernspeicher', '1949', '', '', 'kernspeicher.png', 'Kernspeicher', 'magnetband', 'festplatte', 1, '', 'Die Geschichte des Kernspeichers', 'Speicher'),
+(71, 'festplatte', 'Festplatte', '1956', '1998', '', 'festplatte.png', 'Festplatte', 'kernspeicher', 'speicherchip', 1, '', 'Die Geschichte der Festplatte', 'HDD, SSD'),
 (72, 'speicherchip', 'Speicherchip', '1959', '', 'NEC Japan 8633 kb', 'speicherchip.png', 'Speicherchip', 'festplatte', 'integrierterSpeicher', 1, '', 'Die Geschichte des Speicherchips', ''),
 (73, 'integrierterSpeicher', 'Integrierter Schaltkreis', '1959', '', '', 'integrierter.png', 'Integrierter Schaltkreis', 'speicherchip', 'swafer', 1, '', 'Die Geschichte des integrierten Schaltkreises', ''),
 (74, 'swafer', 'Wafer', '1960', '', '', 'wafer.png', 'Wafer', 'integrierterSpeicher', 'diskette', 1, '', 'Die Geschichte des Wafers', ''),
 (75, 'diskette', 'Diskette', '1969', '2002', 'Externes Laufwerk', 'diskette.png', 'Diskette', 'swafer', 'cd', 1, '', 'Die Geschichte  der Diskette', ''),
-(76, 'cd', 'CD - ROM', '1979', '2000', 'crd-8481B', 'cd.png', 'CD - ROM', 'diskette', 'usbstick', 1, '', 'Die Geschichte der CD-ROM', ''),
-(77, 'usbstick', 'USB - Stick', '1996', '', '16 mb', 'usbstick.png', 'USB - Stick', 'cd', NULL, 1, '', 'Die Geschichte des USB- Sticks', ''),
+(76, 'cd', 'CD - ROM', '1979', '2000', 'crd-8481B', 'cd.png', 'CD - ROM', 'diskette', 'usbstick', 1, '', 'Die Geschichte der CD-ROM', 'CD, DVD, Bluray'),
+(77, 'usbstick', 'USB - Stick', '1996', '', '16 mb', 'usbstick.png', 'USB - Stick', 'cd', NULL, 1, '', 'Die Geschichte des USB- Sticks', 'Universal serial Bus'),
 (78, 'relaisnachbau', 'Nachbau Relairechner', '2017', '', 'Nachbau Relaisrechner', 'relaisnachbau.png', 'Nachbau Relaisrechner', 'halbaddierer', 'roehren', 1, '', 'Nachbau eines Relaisrechners', ''),
 (79, 'transistornachbau', 'Nachbau Transistorrechner', '2017', '', 'Nachbau Transistorrechner', 'transistornachbau.png', 'Nachbau Transistorrechner', 'halbaddierer', 'roehren', 1, '', 'Nachbau eines Transistorrechners', '');
 
 --
--- Indizes der exportierten Tabellen
+-- Indexes for dumped tables
 --
 
 --
--- Indizes für die Tabelle `content`
+-- Indexes for table `content`
 --
 ALTER TABLE `content`
   ADD UNIQUE KEY `ID_2` (`ID`),
   ADD KEY `ID` (`ID`);
 
 --
--- AUTO_INCREMENT für exportierte Tabellen
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT für Tabelle `content`
+-- AUTO_INCREMENT for table `content`
 --
 ALTER TABLE `content`
   MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT COMMENT 'unique id', AUTO_INCREMENT=80;
