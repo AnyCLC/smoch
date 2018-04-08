38302f43aea55391a062c249f2a856d0c64d95e3
pheek <git@gress.ly>
2018-04-08 06:50:01 +0200

    lochkarte geschichte

--- site/artikel_html/lochkarte.php
+++ site/artikel_html/lochkarte.php
@@ -1,7 +1,5 @@
 <h3>Aufbau</h3>
 <p>
-Der Erfinder der Lochkarte ist Herman Hollerit. Er liess seine Erfindung 1884 patentieren.<br>
-Er experimentierte in den folgenden Jahren mit verschiedenen Lochkartenformaten und Anordnungen.<br>
 1886 wurden in Balimore (Maryland) Karten eingesetzt, die jeweils an den beiden  Längsseiten drei Lochreihen mit insgesamt 192 Lochpositionen hatten. Es wurde mit einer Lochzange gelocht.<br>
 </p>
 <?php includeImage(); ?>
@@ -18,6 +16,12 @@ Solange die Speicherung auf magnetischen Datenträgern nicht günstiger wurden, 
 Sogar zum Laden von Programmen wurden Lochkarten verwendet.<br>
 </p>
 
+     <h3>Geschichte</h3>
+     Der Erfinder der Lochkarte ist Herman Hollerit. Er liess seine Erfindung 1884 patentieren.<br>
+Er experimentierte in den folgenden Jahren mit verschiedenen Lochkartenformaten und Anordnungen.<br>
+
+
+     
      <h3>Weitere Informationen</h3>
 <p>
     Weitere Informationen auf <a class="link" href="http://de.wikipedia.org/wiki/Lochkarte">Wikipedia/Lochkarte</a>
