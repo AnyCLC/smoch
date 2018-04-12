b628ff65969c085dafcaf550bb0f9d0e3b3dcd71
pheek <git@gress.ly>
2018-04-12 14:15:22 +0200

    Text Amphora verbessert

--- site/artikel_html/amphore.php
+++ site/artikel_html/amphore.php
@@ -1,22 +1,30 @@
 <h3>Aufbau</h3>
-<p>
-Eine Amphore oder auch Amphora (griech. Für "zweihenkliges Tongefäss") ist ein Krug mit zwei Henkeln.
-Sie besteht meistens aus Ton, aber auch aus Metall (Bronze, Silber, Gold).<br>
-Durch diese zwei Henkel sollte das Tragen erleichtert werden. Amphoren zählt man als antike Vasen.</p>
+<p>Eine Amphore oder auch Amphora (griech. für "zweihenkliges Tongefäss") ist ein Krug mit zwei Henkeln.
+	Sie besteht meistens aus Ton. Es existieren aber auch Amphoren aus Metall (Bronze, Silber, Gold).<br>
+	Durch diese zwei Henkel sollte das Tragen erleichtert werden. Amphoren zählt man als antike Vasen.</p>
 
 <?php includeImage(); ?>
-<h3>Funktionsweise</h3>
-<p>
-Die Amphore war auch ein Hohlmaß im römischen Reich. Eine Amphora betrug ca. 26 Liter.<br>
-</p>
 
+<h3>Funktionsweise</h3>
+<p>Durch die Öffnung am oberen Ende konnten Waren (Esswaren, Getränke, ...) eingefüllt aber auch wieder hinausgenommen werden.</p>
+	 
 <h3>Verwendung</h3>
-<p>
-Amphoren wurden in der Antike als Speicher- und Transportgefäss für z. B. Wein, Oliven, Milch und Honig benutzt. Je nach Inhalt war das Volumen unterschiedlich. Das Fassungsvermögen betrug zwischen 5 und 50 Liter.<br>
-Heute werden Amphoren als Vasen benutzt. Eine grosse Rolle spielt die Amphore bei der Herstellung spezieller Weine, wie z. B. "Amphorenwein".<br>
-</p>
+<p>Amphoren wurden in der Antike als Speicher- und Transportgefäss für z. B. Wein, Oliven, Milch und Honig benutzt.
+	Je nach Inhalt war das	Volumen unterschiedlich. Das Fassungsvermögen betrug zwischen 5 und 50 Liter.<br>
+	Heute werden Amphoren als Vasen benutzt. Eine grosse Rolle spielt die Amphore bei der Herstellung spezieller Weine, wie z. B. "Amphorenwein".</p>
+	 
+<h3>Fun Facts</h3>
+<img src='../expo_images/krug.png' alt='Plastiknachbildung einer Amphore' title='Krug aus dem 3D-Drucker' />
+	<p>Die zweite Abbildung zeigt eine Nachbildung einer Amphore aus dem 3D-Drucker. Diese wurde zunächst mit dem Programm <a href='https://www.blender.org'>Blender</a> modelliert.<p>
+<p>Amphoren waren häufig auch beschritftet. Die Aufschriften geben
+	heutigen Archäologen häufig Datierungsmöglichkeiten und Indizien
+	zu vielfältigen Einsatzbereichen.
+	Da bei verschließbaren Amphoren nicht gesehen werden konnte, was darin enthalten war, mussten die Amphoren irgendwie
+	bezeichnet werden; und ... ohne Symbole und Schriften, wären heutige Computer undenkbar.
+	Somit ist die Amphore ein mehrfacher Speicher: Inhalt und Beschriftung.
+	</p>
 
 <h3>Weitere Informationen</h3>
-<p>
-    Weitere Informationen auf <a class="link" href="http://de.wikipedia.org/wiki/Amphore">Wikipedia/Amphore</a>
+<p>Die Amphore war auch ein Hohlmaß im römischen Reich. Eine römische Amphora betrug ca. 26 Liter.<br>
+	Weitere Informationen auf <a class="link" href="http://de.wikipedia.org/wiki/Amphore">Wikipedia/Amphore</a>
 </p>
