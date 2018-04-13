f888a3602f53e91f764285f75d676feba323954c
pheek <git@gress.ly>
2018-04-13 07:04:13 +0200

    code Verbesserung + Auslagerung Schrift Exponat ins CSS

--- site/res/functions.php
+++ site/res/functions.php
@@ -7,52 +7,55 @@
  */
 
 function includeImage() {
-    global $bildpfad;
-    global $bildlegende;
-    echo    "<div class='imgwrapper'>" .
-        "<img src='../expo_images/$bildpfad' class='expoimg' alt='$bildlegende' title='$bildlegende'/>" .
-        "</div>";
+	global $bildpfad;
+	global $bildlegende;
+	echo  "<div class='imgwrapper'>" .
+	      "<img src='../expo_images/$bildpfad' class='expoimg' alt='$bildlegende' title='$bildlegende'/>" .
+	      "</div>";
 }
 
 
 function underConstruction() {
-    echo "<div><img src='../img/ucMini.png' alt='teleworker' title='under Construction'/><br />Under Construction</div>";
+	echo "<div><img src='../img/ucMini.png' alt='teleworker' title='under Construction'/><br />Under Construction</div>";
 }
+
 
 /** 
  * Text für vorhandene Exponate.
  */
 function istImMuseumText($modellObj, $jahrzahlObj, $vorhanden) {
-    if(! empty ($modellObj) || ! empty ($jahrzahlObj) || ! empty($vorhanden)) {
-        echo "<hr />\n";
-    }
-    if($vorhanden > 0) {
-        echo "<br />Hierzu können Sie ein Exponat im <b>SANTIS Museum of Computing History</b> betrachten.<br />\n";
-    }
-    if(! empty ($modellObj) && strlen($modellObj) > 0) {
-        if($vorhanden) {
-            echo "<br />Bestauenen Sie bei uns: ";
-        } else {
-            echo "<br />Abbildung zeigt bzw. Text beschreibt: ";
-        }
-        echo  "<b><span style='color: #036;'>" . $modellObj;
-        if(! empty($jahrzahlObj)) {
-            echo " aus dem Jahr $jahrzahlObj";
-        }
-        echo "</span></b>.<br />\n";
-    }
+	if(! empty ($modellObj) || ! empty ($jahrzahlObj) || ! empty($vorhanden)) {
+		echo "<hr />\n";
+	}
+	if($vorhanden > 0) {
+		echo "<br />Hierzu können Sie ein Exponat im <b>SANTIS Museum of Computing History</b> betrachten.<br />\n";
+	}
+	if(! empty ($modellObj) && strlen($modellObj) > 0) {
+		if($vorhanden) {
+			echo "<br />Bestauenen Sie bei uns: ";
+		} else {
+			echo "<br />Abbildung zeigt bzw. Text beschreibt: ";
+		}
+		echo  "<span class='museumsobjekt'>" . $modellObj;
+		if(! empty($jahrzahlObj)) {
+			echo " aus dem Jahr $jahrzahlObj";
+		}
+		echo "</span>.<br />\n";
+	}
 }
 
 /**
  * @author git@gress.ly
  * @date   2018-04-12
+ *
  * Create a Keywordlist containing the IDUrl and a comma separated list of
  * given keywords from the database.
  * If the title ($title) differs from the $IDurl, the title is added to the
  * keywords as well.
- * @param IDurl   : string id of the "exponate"
- * @param title   : Title in the database
- * @param keywords: Keywords from the database
+ *
+ * @param   IDurl   : string id of the "exponate"
+ * @param   title   : Title in the database
+ * @param   keywords: Keywords from the database
  * @returns komma separated list of keywords
  */
 function keyWordList($IDurl, $title, $keywords) {
