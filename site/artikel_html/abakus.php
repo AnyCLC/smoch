4d68fc0262e678da64ee04481f44dfb9bcea83e4
Unknown <marc.sobe@gmail.com>
2018-04-17 15:57:20 +0200

    Abakus class korrigiert
    zitsrc wird zit src

--- site/artikel_html/abakus.php
+++ site/artikel_html/abakus.php
@@ -2,7 +2,7 @@
      
 <h3>Aufbau</h3>
 <p class='zit copy'>Der Abakus besteht aus einem Rahmen und aus mehreren Kugeln, meistens aus Holz oder Glasperlen.<br></p>
-<p class ='zitsrc'> Wickipedia 2018 </p>
+<p class ='zit src'>Wickipedia 2018 </p>
 <p>
 Der Abakus, der älteren Generation, bestand aus Steinen.<br>
 Die Variante mit den Kugeln wird auch <i>russischer Abakus</i> genannt, weil er dort das erste Mal vorkam.<br>
@@ -21,11 +21,11 @@ Man kann sogar die Quadrat- und Kubikwurzeln ziehen, was schon wesentlich komple
 <h3>Verwendung</h3>
 <p class='zit copy'>
 Der Abakus (lat. für die Tafel, das Brett) ist ein 3000 Jahre altes Rechenhilfsmittel und wurde vor allem bei kleinen Geschäften verwendet, da er leicht zu bedienen war.
-<p class ='zitsrc'> Wickipedia 2018 </p></p>
+<p class ='zit src'> Wickipedia 2018 </p></p>
 
                                                                  <h3>Fun Facts</h3>
 <p class='zit copy'>Da man höhere Rechenoperationen, mit den römischen Zahlen nicht durchführen konnte, gab es im Römischen Reich eine leicht abgewandelte Version des Abakus.</p>
-<p class ='zitsrc'> Wickipedia 2018 </p>
+<p class ='zit src'> Wickipedia 2018 </p>
                                                                  <h3>Weitere Informationen</h3>
 <p>
 Weitere Informationen auf <a class="link" href="http://de.wikipedia.org/wiki/Abakus_%28Rechenhilfsmittel%29">Wikipedia/Abakus</a>
