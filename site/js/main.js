5817437d41a1ff0a051b05a854de7e68a3b90e38
pheek <git@gress.ly>
2018-04-17 16:53:02 +0200

    Stickinavi verbessert, sodass die Breite auch stimmt, wenn das Fenster mit resize verändert wurde.

--- site/js/main.js
+++ site/js/main.js
@@ -3,22 +3,24 @@
  * The sticky navigation is as wide as the "headwrapper".
  */
 var navTopDistance = 0;
+function setTopDistance() {
+    navTopDistance = document.getElementById("navwrapper").offsetTop;
+}
+
+
 function stickyNavi() {
 	var scroll = window.pageYOffset;
 	var nav    = document.getElementById("navwrapper");
-	if (scroll > nav.offsetTop) {
+	if (scroll > navTopDistance) {
 		// make it sticky:
 		nav.classList.add("stickyNav");
-		// make it the same width as the headwrapper
-		var headwrapper = document.getElementById('headwrapper');
-		var width       = headwrapper.offsetWidth;
-		nav.style["width"] = width + "px";
 	} else {
 		nav.classList.remove("stickyNav");
 	}
-}
-function setTopDistance() {
-    navTopDistance = document.getElementById("navwrapper").offsetTop;
+	// make it the same width as the headwrapper
+	var headwrapper = document.getElementById('headwrapper');
+	var width       = headwrapper.offsetWidth;
+	nav.style["width"] = width + "px";
 }
 
 // Use if user scrolls or changes window size:
