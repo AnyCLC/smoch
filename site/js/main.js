770bbcff6c47aa5bfa8263c82a482ece907e6c38
Nathan Péray <nathan.peray@santismail.ch>
2018-04-17 13:54:33 +0200

    Revert "CSS... Navi richtig positioniert, Sticky nav verbessert"
    This reverts commit 1158a8e8ef666ee4593e05a8b15e6b99a5245e44.

--- site/js/main.js
+++ site/js/main.js
@@ -5,21 +5,18 @@
 function stickyNavi() {
 	var scroll = window.pageYOffset;
 	var nav    = document.getElementById("navwrapper");
-        // make it the same width as the headwrapper
-        
-	if (scroll >= nav.offsetTop) {
+	if (scroll > nav.offsetTop) {
 		// make it sticky:
 		nav.classList.add("stickyNav");
-		
+		// make it the same width as the headwrapper
+		var headwrapper = document.getElementById('headwrapper');
+		var width       = headwrapper.offsetWidth;
+		nav.style["width"] = width + "px";
 	} else {
 		nav.classList.remove("stickyNav");
 	}
-        var headwrapper = document.getElementById('headwrapper');
-	var width       = headwrapper.offsetWidth;
-        nav.style["width"] = width + "px";
 }
 
 // Use if user scrolls or changes window size:
-window.addEventListener('load', () => { stickyNavi(); });
 window.addEventListener('scroll', () => { stickyNavi(); });
 window.addEventListener('resize', () => { stickyNavi(); });
