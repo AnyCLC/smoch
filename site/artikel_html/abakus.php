b8059353e804ac6088261d2aa59c059c6f65dc50
pheek <git@gress.ly>
2018-04-21 21:38:29 +0200

    Artikeltexte

--- site/artikel_html/abakus.php
+++ site/artikel_html/abakus.php
@@ -1,31 +1,31 @@
 <div class='zitwarn'></div>
      
 <h3>Aufbau</h3>
-<p class='zit copy'>Der Abakus besteht aus einem Rahmen und aus mehreren Kugeln, meistens aus Holz oder Glasperlen.<br></p>
-<p class ='zit src'>Wickipedia 2018 </p>
+<p class='zit copy'>Der Abakus besteht aus einem Rahmen und aus mehreren Kugeln, meistens aus Holz oder Glasperlen.</p>
+<p class ='zit src'>Wikipedia 2018</p>
+     
 <p>
-Der Abakus, der älteren Generation, bestand aus Steinen.<br>
+Der Abakus der älteren Generation bestand aus Steinen.<br>
 Die Variante mit den Kugeln wird auch <i>russischer Abakus</i> genannt, weil er dort das erste Mal vorkam.<br>
 Das verwandte Rechenbrett bestand aus Münzen oder Rechensteinen.<br>
-Die einzelnen Kugeln stellen eine bestimmte Zahl dar.<br>
-Der Wert, der Zahlen läuft nach einem Stellenwertsystem.<br>
+Die einzelnen Kugeln stellen einen bestimmten Stellenwert dar.<br>
+Der Wert der Zahlen läuft nach einem Stellenwertsystem.<br>
 </p>
 
 <?php includeImage(); ?>
-
                                                                  <h3>Funktionsweise</h3>
 <p>
 Indem die Kugeln von der einen Seite der Spalte zur anderen verschoben werden, kann mit einem Abakus addiert, subtrahiert, multipliziert sowie dividiert werden.<br>
-Man kann sogar die Quadrat- und Kubikwurzeln ziehen, was schon wesentlich komplexer ist, als das Rechnen mit den vier Grundoperatoren.</p>
+Man kann sogar die Quadrat- und Kubikwurzeln ziehen, was schon wesentlich komplexer ist als das Rechnen mit den vier Grundoperatoren.</p>
                                                                  
 <h3>Verwendung</h3>
 <p class='zit copy'>
-Der Abakus (lat. für die Tafel, das Brett) ist ein 3000 Jahre altes Rechenhilfsmittel und wurde vor allem bei kleinen Geschäften verwendet, da er leicht zu bedienen war.
-<p class ='zit src'> Wickipedia 2018 </p></p>
+Der Abakus (lat. für die Tafel, das Brett) ist ein 3000 Jahre altes Rechenhilfsmittel und wurde vor allem bei kleinen Geschäften verwendet, da er leicht zu bedienen war.</p>
+<p class ='zit src'> Wikipedia 2018 </p>
 
                                                                  <h3>Fun Facts</h3>
 <p class='zit copy'>Da man höhere Rechenoperationen, mit den römischen Zahlen nicht durchführen konnte, gab es im Römischen Reich eine leicht abgewandelte Version des Abakus.</p>
-<p class ='zit src'> Wickipedia 2018 </p>
+<p class ='zit src'> Wikipedia 2018 </p>
                                                                  <h3>Weitere Informationen</h3>
 <p>
 Weitere Informationen auf <a class="link" href="http://de.wikipedia.org/wiki/Abakus_%28Rechenhilfsmittel%29">Wikipedia/Abakus</a>
--- site/artikel_html/bitRelaisNachbau.php
+++ site/artikel_html/bitRelaisNachbau.php
@@ -1,5 +1,20 @@
-<?php underConstruction(); ?>
-<?php includeImage();      ?>
+<h3>Aufbau</h3>
 <img src='../expo_images/bitRelaisNachbau_schema.png'
      alt='Relais Latch als Schaltschema: Der Ausgang des geschalteten Relais ist mit dem Input verbunden'
      title='Relais-Latch, um ein Bit zu speichern' />
+    <p>Das Relais-Latch (Latch = Schnappschloss) wird mit einem eizigen Relais gebaut (s. elektronisches Schema).</p>
+
+<h3>Funktionsweise</h3>
+<?php includeImage();      ?>
+    <p>Die Schaltung handelt sich um eine einfache, wenn auch <i>stromfressende</i> Variante.</p>
+    <p>Dabei wird der Strom, der beim angezogenen Relais fließt, gleich wieder verwendet, um das Relais «einzuschalten»; so bleibt es nach einmaligem Schalten <i>eingeschnappt</i>, wie ein Schnappschloss, eben.</p>    
+                                                                                                                             <p>Um das Bit wieder zu löschen muss die Zuleitung unterbrochen werden (Aus-Taster in der Grafik). Dies geschieht in unserer Schaltung einfach mit einem Taster; in einem Schaltnetz, wird jedoch dazu i.&nbsp;d.&nbsp;R. ein anderes Relais verwendet.</p>
+
+
+<h3>Verwendung</h3>
+<p>Das Relais-Latch wird verwendet, um ein einzelnes Bit zu speichern.</p>
+
+<h3>Fun Facts</h3>
+<p>Im Gegensatz zur hier beschriebenen Schalaltung "Relais-Latch" gibt es eigens dafür geschaffene "<a href='https://en.wikipedia.org/wiki/Relay'>Latching Relais</a>", welche zum Umschalten lediglich einen kurzen Impuls benötigen.</p>
+
+
