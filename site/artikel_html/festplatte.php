97a4cb59d976f626346892a8ad02c0020b0a4df1
ZaLightning <risto.stjepanic@gmail.com>
2018-10-03 10:38:07 +0200

    Festplatte Angepasst

--- site/artikel_html/festplatte.php
+++ site/artikel_html/festplatte.php
@@ -1,33 +1,32 @@
-<div class='zitwarn'></div>
-
 <h3>Aufbau</h3>
 <p>
-Eine Festplatte ist ein Speichermedium, um grosse Datenmengen abzuspeichern.
-Es gibt sie intern als Hauptspeichermedium jedes Computers, oder als externes Medium zur Datensicherung oder Datenauslagerung.
-Es gibt verschiedene Varianten der Festplatte.
+  Eine Festplatte ist ein Speichermedium um grosse Datenmengen abzuspeichern. Es gibt sie intern als Hauptspeichermedium jedes Computers, oder als externes Medium zur Datensicherung oder Datenauslagerung. Es gibt auch verschiedene Varianten der Festplatte.
+
+  Die Bekannteste ist die HDD (Hard Disk Drive). Sie besitzt eine rotierende Magnetscheibe die durch einen sich bewegenden Lesekopf abgelesen wird, ähnlich wie bei einem Plattenspieler.
+
+  In den Letzten Jahren kam die SSD (Solid-State-Drive) immer mehr auf, Sie besitzt keine bewegenden Teile mehr, sie benutzt einen Flashspeicher im Grunde ein grosser USB-Stick.
+
+  Die Vorteile der SSD gegenüber der HDD sind folgende:
+
+  Schnellere Datenauslese und Abspeicherung
+
+  Die Daten sind sicherer, da es keinen sich bewegenden Lesekopf mehr gibt, der auf die Magnetscheibe prallen kann.
+
+  Die Nachteile sind:
+
+  Hoher Preis, wobei dieser langsam sinkt.
 </p>
 <?php includeImage(); ?>
 
      <p>Die Festplatte wird auch Harddisk oder HDD (Hard Disk Drive) genannt. Sie besitzt eine rotierende Magnetscheibe die durch einen sich bewegenden Lese-/Schreibkopf wahlweise beschrieben oder ausgelesen werden kann; ähnlich wie bei einem Plattenspieler.</p>
 
 <h3>Funktionsweise</h3>
-<p>Eine Festplatte ist im Wesentlichen eine magnetisierbare runde und harte Scheibe.<br />
+<p>Die Funktionsweise einer Festplatte ist bei HDD oder SSD in den Grundzügen gleich. Es werden Daten gespeichert, und sie können wieder ausgelesen werden.</p>
 
-Darauf werden die Zustände Wahr/Falsch bzw. Eins/Null durch ummagnetisieren durch den Schreibkopf definiert bzw. den Lesekopf wieder herausgefunden.<br />
-
-    Damit das Betriebssystem «weiß», wo die Dateien und Verzeichnisse abgelegt sind, werden Festplatten auf spezielle Weise «formattiert»; dabei wird die Platte in eindeutig identifizierbare Spuren und Sektoren eingeteilt.</p>
-
-     
 <h3>Verwendung</h3>
-<p>
-Verwendet werden Festplatten als Speichermedium großer Datenmengen.
-Externe Festplatten dienen auch als Backupspeicher.<br />
+<p>Verwendet werden Festplatten als Speichermedium vieler Daten. Externe Festplatten dienen auch als Backupspeicher.</p>
 
-    Da im Gegensatz zu Magnetbändern die Platte fast Wahlweise gelesen und beschrieben werden kann, eignet sich die Festplatte für das Ablegen und verändern vieler kleinerer Dateien (z. B. Betriebssystem, Softwareentwicklung, ...). Da auf der HDD aber auch große Datenmengen und große Dateien gespeichert werden können, hat die Harddisk das Magnetband in vielen Anwendungsbereichen verdrängt.</p>
-
-                                                                                                                                               
 <h3>Weitere Informationen</h3>
 <p>
-Weitere Informationen finden Sie 
-<a href="https://de.wikipedia.org/wiki/Festplattenlaufwerk">hier</a>.
+Weitere Informationen finden Sie <a href="Weitere Informationen finden Sie hier.">hier</a>.
 </p>
